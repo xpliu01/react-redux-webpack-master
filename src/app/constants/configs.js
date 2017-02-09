a7575715c69f8d4954f815559f0b1aee32bd8db7
雕鹏 <923512260@qq.com>
2017-02-09 18:06:18 +0800

    配置文件更新
    配置文件更新

--- src/app/constants/configs.js
+++ src/app/constants/configs.js
@@ -4,15 +4,7 @@
  * 创建原因：
  */
 'use strict';
-export  const API_DEV="http://10.10.1.108:8034/api/"; //开发环境API
-export  const API_PRO="http://121.43.104.136:8022/api/"; //生产环境API
-export  const API_TIMEOUT_DEV=5000; //开发环境API
-export  const API_TIMEOUT_PRO=5000; //开发环境API
-
-
-
-export  const API_YDT_DEV="http://121.43.104.136:8022/api/"; //生产环境API(移动通)
-//export  const API_YDT_DEV="http://10.10.1.6:8057/api/"; //开发环境API(移动通)
-export  const API_YDT_PRO="http://121.43.104.136:8022/api/"; //生产环境API(移动通)
-export  const API_YDT_TIMEOUT_DEV=15000; //开发环境API(移动通)
-export  const API_YDT_TIMEOUT_PRO=15000; //生产环境API(移动通)
+export  const API_DEV="http://localhost:8080/api/";
+export  const API_PRO="http://localhost:8080/api/";
+export  const API_TIMEOUT_DEV=5000;
+export  const API_TIMEOUT_PRO=5000;
