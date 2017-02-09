a53cdb0e1e84c50c67e098f30f9e4007eea464b7
雕鹏 <923512260@qq.com>
2017-02-09 18:09:55 +0800

    配置文件更新
    配置文件更新

--- src/app/constants/systemActionTypes.js
+++ src/app/constants/systemActionTypes.js
@@ -1,20 +1,8 @@
 /*
-* 系统模块Actiion
+* 系统Actiion
 * */
 export const SYSTEM_LOGIN_S = 'SYSTEM_LOGIN_S';//登录成功
 export const SYSTEM_LOGIN_F = 'SYSTEM_LOGIN_F';//登录失败
-export const SYSTEM_LOGIN_READING = 'SYSTEM_LOGIN_READING';//登录加载中
-export const SYSTEM_USE = 'SYSTEM_USE';//系统是否初次使用
-export const SYSTEM_ENTERPRISE = 'SYSTEM_ENTERPRISE';//重置企业数据
-
-export const SYSTEM_BIND = 'SYSTEM_BIND';//绑定操作
-export const SYSTEM_CHECKLOGIN='SYSTEM_CHECKLOGIN';//登录验证(是否绑定,是否登录)
-export const SYSTEM_MSGUSER = 'SYSTEM_MSGUSER';//个人消息目标用户
-export const SYSTEM_MSGGROUP = 'SYSTEM_MSGGROUP';//个人消息目标群租
-export const SYSTEM_SETUSERICON = 'SYSTEM_SETUSERICON';//个人消息目标群租
-
-
-export const SYSTEM_PERSON = 'SYSTEM_PERSON';//系统人员操作
 
 
 
